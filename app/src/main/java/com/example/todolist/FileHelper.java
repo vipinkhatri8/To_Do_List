a2534d2bfb551901f4d9253c426285b11c6aa6f5
vipin <vipinkhatri444@gmail.com>
2023-09-21 00:03:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/todolist/FileHelper.java
@@ -0,0 +1,53 @@
+package com.example.todolist;
+
+import android.content.Context;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+public class FileHelper {
+
+    public static final  String FILENAME ="listinfo.dat";
+
+    public static  void  writeData(ArrayList<String> item, Context context)
+    {
+        try {
+            FileOutputStream fos = context.openFileOutput(FILENAME,Context.MODE_PRIVATE);
+            ObjectOutputStream oas  = new ObjectOutputStream(fos);
+            oas.writeObject(item);
+            oas.close();
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public  static ArrayList<String> readData(Context context)
+    {
+        ArrayList<String> itemList = null ;
+        try {
+            FileInputStream fis = context.openFileInput(FILENAME);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            itemList = (ArrayList<String>) ois.readObject();
+
+
+        } catch (FileNotFoundException e) {
+            itemList = new ArrayList<>();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    return itemList;
+    }
+
+}
